6c085c292fca614cfc681ea6531543312c950397
nizamuddin0172 <neom01723441081@gmail.com>
2024-06-08 12:12:27 +0600

    add navbar and theme button toggle to handleClick

---
+++ src/components/Bloges/Bloges.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bloges = () => {
+    return (
+        <div>
+            <h1>This is a Bloge Page</h1>
+        </div>
+    );
+};
+
+export default Bloges;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <div className="h-16">
+            <Navbar></Navbar>
+            </div>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
